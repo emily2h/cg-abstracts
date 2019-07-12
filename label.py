0c06f7040f81d04a7b996c6495f5b923c6fc54af
Emily <emhuang321@yahoo.com>
2019-07-12 14:41:10 -0700

    uploading more data

--- label.py
+++ label.py
@@ -1,13 +1,33 @@
 import pandas as pd
 
 
-f = open("keywords0.txt", "r")
-keywords0 = f.readlines()
-for w in keywords0:
-    w= w.strip()
+userdir = "/home/emily2h/Summer/data"
+filename = "vci_1543_abs_tit_key_apr_1_2019_train.csv"
+
+df = pd.read_csv("{}/{}".format(userdir, filename), sep='\t', header=0)
+keywords0 = open("keywords0.txt", "r").read().splitlines()
+
+
 print(keywords0)
+#print(type(df.Abstract[0]))
 
 
 
-#def exp_studies_keywords():
+def exp_studies_keywords(abstract):
+    for w in keywords0:
+        if w in abstract:
+            return 1
+    return 0
+
+labels = []
+for a in df.Abstract:
+    labels.append(exp_studies_keywords(a))
+
+print(labels.count(0))
+print(labels.count(1))
+
+df['label0'] = labels
+#print(df)
+
+
     
