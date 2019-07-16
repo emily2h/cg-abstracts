29c0153c918dec5463eece1a5a267061feed579e
Emily <emhuang321@yahoo.com>
2019-07-16 12:40:02 -0700

    majority algorithm

--- adj.py
+++ adj.py
@@ -1,13 +1,33 @@
 import spacy
+import re
 import pandas as pd
 from collections import Counter
 from collections import OrderedDict 
+from nltk import RegexpParser
 import numpy as np
 import seaborn as sn
 import matplotlib.pyplot as plt
 
 c0, c1, c2, c3, c4 = Counter(), Counter(), Counter(), Counter(), Counter()
+d0, d1, d2, d3, d4 = Counter(), Counter(), Counter(), Counter(), Counter()
+#def check_for_percents():
+#    percents = lambda row: re.find(r)
 
+def rem_keywords():
+    remKeyWords = lambda row: re.sub(r'\|\|.*\|\|', '', row) 
+    df['Abstract'] = df['Abstract'].apply(remKeyWords)
+
+def count_noun_chunks(word, i):
+    d = d0
+    if i == 1:
+        d = d1
+    if i == 2:
+        d = d2
+    if i == 3:
+        d = d3
+    if i == 4:
+        d = d4
+    d[word.text.lower()] += 1
 
 def check_adj(word, i):
     c = c0
@@ -19,7 +39,6 @@ def check_adj(word, i):
         c = c3
     if i == 4:
         c = c4
-
     if word.pos_ == "ADJ":
         c[word.lemma_.lower()] += 1
         #c[word.text.lower()] += 1
@@ -60,16 +79,18 @@ nm = pd.DataFrame(cooc_mat)
 # 2 and 3 commonly cooccur with 4
 print(nm)
 ax = sn.heatmap(nm, square=True, center=True)
-plt.show()
-
+#plt.show()
+rem_keywords()
+print(df.Abstract[11])
 
 nlp = spacy.load('en')
-
 for i in range(5):
     df_i = df[df.Classification.str.contains("{}".format(i))]
     print(df_i.head())
     pos = df_i['Abstract'].apply(nlp)
     for abstract in pos:
+        for np in abstract.noun_chunks:
+            count_noun_chunks(np, i)
         for w in abstract:
             check_adj(w, i)
 print("c0",c0.most_common(10))
@@ -77,6 +98,12 @@ print("c1",c1.most_common(10))
 print("c2",c2.most_common(10))
 print("c3",c3.most_common(10))
 print("c4",c4.most_common(10))
+
+print("d0",d0.most_common(10))
+print("d1",d1.most_common(10))
+print("d2",d2.most_common(10))
+print("d3",d3.most_common(10))
+print("d4",d4.most_common(10))
     
 # make dataframe of adjectives, use counter on classifications of that?
 
--- label.py
+++ label.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import re
 import random
 from sklearn.metrics import confusion_matrix
+from collections import Counter
 
 
 userdir = "/home/emily2h/Summer/cg-abstracts"
@@ -21,6 +23,25 @@ def exp_studies_keywords(abstract):
             return 1
     return 0
 
+# labeling function 2
+def find_percentage(abstract):
+    result = re.findall(r'\d{0,2}\-?\d{1,3}\%', abstract)
+    if len(result) < 1:
+        return 0
+    else:
+        return 1
+
+"""
+# returns random label
+def random_label3():
+    return random.randrange(0, 2)
+"""
+
+def tiebreaker(majority):
+    newmajority = min(majority)  # change from min to max for diff result
+    return [newmajority]
+
+
 def score():
     count = 0
     classify = lambda row: 1\
@@ -30,10 +51,23 @@ def score():
     print(df['Accuracy'].sum()/len(df['Accuracy']))
 
 
-labels = []
+tlabels = []
 for a in df.Abstract:
-    labels.append(exp_studies_keywords(a))
-# labels = [random.randrange(0, 2) for _ in range(0, len(df['Abstract']))] 
+    label_i = Counter()
+    label_i[exp_studies_keywords(a)] += 1
+    label_i[find_percentage(a)] += 1
+    print(label_i)
+    majority = label_i.most_common()
+    print(majority)
+    if(len(majority) > 1):
+        majority = tiebreaker(majority)
+    tlabels.append(majority)
+
+
+print(tlabels)
+labels = [x[0][0] for x in tlabels]
+
+# labels = [random.randrange(0, 2) for _ in range(0, len(df['Abstract']))]  # for randomly assigned labels
 #print(labels)
 
 print(labels.count(0))
